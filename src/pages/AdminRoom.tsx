bbf0a98cd3b2bdf801807b00202572fcbfcf6778
Lucas Dantas <lucasdantasmoura01@gmail.com>
2021-07-31 11:46:36 -0300

    Ajuste nas questions para aparecer as funcionalidades

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -10,6 +10,8 @@ import { database } from '../services/firebase'
 
 import logoImg from '../assets/images/logo.svg'
 import deleteImg from '../assets/images/delete.svg'
+import checkImg from '../assets/images/check.svg'
+import answerImg from '../assets/images/answer.svg'
 
 import '../styles/room.scss'
 
@@ -19,23 +21,35 @@ type RoomParams = {
 }
 
 export function AdminRoom() {
-  //  const { user } = useAuth()
+    //  const { user } = useAuth()
     const history = useHistory()
     const params = useParams<RoomParams>()
     const roomId = params.id
     const { title, questions } = useRoom(roomId)
 
     async function handleEndRoom() {
-       await database.ref(`rooms/${roomId}`).update({
-           endedAt: new Date()
-       })
-       history.push('/')
+        await database.ref(`rooms/${roomId}`).update({
+            endedAt: new Date()
+        })
+        history.push('/')
     }
 
-  async function handleDeleteQuestion(questionId: string) {
-       if(window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
+    async function handleDeleteQuestion(questionId: string) {
+        if (window.confirm('Tem certeza que você deseja excluir esta pergunta?')) {
             await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
-       }
+        }
+    }
+
+    async function handleCheckQuestionAsAnswered(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isAnswered: true
+        })
+    }
+
+    async function handleHighlightQuestion(questionId: string) {
+        await database.ref(`rooms/${roomId}/questions/${questionId}`).update({
+            isHighlighted: true
+        })
     }
 
     return (
@@ -58,7 +72,30 @@ export function AdminRoom() {
                 <div className="question-list">
                     {questions.map(question => {
                         return (
-                            <Question key={question.id} content={question.content} author={question.author}>
+                            <Question
+                                key={question.id}
+                                content={question.content}
+                                author={question.author}
+                                isHighlighted={question.isHighlighted}
+                                isAnswered={question.isAnswered}
+                            >
+                                {question.isAnswered && (
+                                    <>
+                                        <button
+                                            type="button"
+                                            onClick={() => handleCheckQuestionAsAnswered(question.id)}
+                                        >
+                                            <img src={checkImg} alt="Marcar pergunta como respondida" />
+                                        </button>
+                                        <button
+                                            type="button"
+                                            onClick={() => handleHighlightQuestion(question.id)}
+                                        >
+                                            <img src={answerImg} alt="Dar destaque à pergunta" />
+                                        </button>
+                                    </>
+                                )
+                                }
                                 <button
                                     type="button"
                                     onClick={() => handleDeleteQuestion(question.id)}
